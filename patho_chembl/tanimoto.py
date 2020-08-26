1b3b185ce808deab448037639de19ad0132a2ae8
Florencia A. Castello <68971292+florenciacastello@users.noreply.github.com>
2020-08-26 10:35:02 -0300

    Add files via upload

--- patho_chembl/tanimoto.py
+++ patho_chembl/tanimoto.py
@@ -3,14 +3,17 @@ from chembl_webresource_client.new_client import new_client
 from io import StringIO
 import pandas as pd
 from pandas import json_normalize
+
+#ejemplo=['COC1=C2C(OC=C2)=NC3=CC(OC)=C(OC)C=C31', 'COC1=C2C(OC=C2)=NC3=C(OC)C4=C(OCO4)C=C31']
+
 def chembl_similarity (handle, tanimoto_similarity):
-	smiles = handle.read().strip()
+	similarity=[]
 	if handle:
-		for x in handle:
-			print(new_client.similarity.filter(smiles = x, similarity = tanimoto_similarity))
-	else:
-		print('No smiles')
-	return 0
+		for smiles in handle:
+			smiles = smiles.strip()
+			similarity=similarity+list(new_client.similarity.filter(smiles = smiles, similarity = tanimoto_similarity).only('molecule_chembl_id',
+			'similarity', 'molecule_structures'))
+	return similarity
 
 def Main():
 	parser = argparse.ArgumentParser()
@@ -20,15 +23,21 @@ def Main():
 	args = parser.parse_args()
 
 	res = chembl_similarity(args.input, args.tanimoto)
+	if not res:
+		print('No result', file=sys.stderr)
+		return 0
 
 	df = pd.DataFrame.from_dict(json_normalize(res), orient='columns')
 	df_final = df[['molecule_chembl_id', 'similarity','molecule_structures.canonical_smiles']]
+	df_nodup= df_final.drop_duplicates(subset=['molecule_chembl_id'])
 	output = StringIO()
-	df_final.to_csv(output) ;
+	df_nodup.to_csv(output) ;
+
 	if args.output:
-		f = open(args.output, "a")
-		f.write(str(df_final))
+		f = open(args.output, "w")
+		f.write(str(df_nodup))
 	else: print(output.getvalue())
+
 
 if __name__=='__main__':
 		Main()
