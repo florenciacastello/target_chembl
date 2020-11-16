6543d8be23c37c2668b391103dfd4d6b061abb4a
Florencia A. Castello <68971292+florenciacastello@users.noreply.github.com>
2020-11-16 12:12:25 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='target_chembl',
-		version='0.0.2',
-		py_modules=['patho_chembl.trg_mol'],
+		version='0.0.3',
+		py_modules=['patho_chembl.trg_mol_funcion'],
 		scripts=['patho_chembl/chembldb_pfam_mech.py', 'patho_chembl/chembldb_pfam_assay.py', 'patho_chembl/mol_trg.py', 'patho_chembl/pfam_df_update.py', 'patho_chembl/pfam_mol_assay.py',
     		'patho_chembl/pfam_mol_mech.py', 'patho_chembl/pfam_trg_sql_assay.py', 'patho_chembl/pfam_trg_sql_mecanism.py', 'patho_chembl/tanimoto.py', 'patho_chembl/trg_mol.py', 'patho_chembl/trg_mol_funcion.py'],
 
