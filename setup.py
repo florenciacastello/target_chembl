1040f7ef801b62b1ae123e6969e7059ca602e2fa
Florencia A. Castello <68971292+florenciacastello@users.noreply.github.com>
2020-11-16 11:39:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(name='target_chembl',
 		version='0.0.1',
 		py_modules=['patho_chembl'],
 		scripts=['patho_chembl/chembldb_pfam_mech.py', 'patho_chembl/chembldb_pfam_assay.py', 'patho_chembl/mol_trg.py', 'patho_chembl/pfam_df_update.py', 'patho_chembl/pfam_mol_assay.py',
-    'patho_chembl/pfam_mol_mech.py', 'patho_chembl/pfam_trg_sql_assay.py', 'patho_chembl/pfam_trg_sql_mecanism.py', 'patho_chembl/tanimoto.py', 'patho_chembl/trg_mol.py'],
+    		'patho_chembl/pfam_mol_mech.py', 'patho_chembl/pfam_trg_sql_assay.py', 'patho_chembl/pfam_trg_sql_mecanism.py', 'patho_chembl/tanimoto.py', 'patho_chembl/trg_mol.py'],
 
 		requires=['requests','argparse', 'chembl_webresource_client', 'pandas', ],
 
